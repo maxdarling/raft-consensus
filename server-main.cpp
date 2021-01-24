f1f0ff5d761bf917c99e2ee0ff15a18f40890491
Ali Saeed <8asaeed@gmail.com>
2021-01-24 00:27:03 -0600

    Added protobufs, but there may be linking issues?

---
+++ server-main.cpp
@@ -0,0 +1,12 @@
+// TODO(ali): get protobufs to work
+#include <iostream>
+#include "raft_RPCs.pb.h"
+
+int main() {
+    // GOOGLE_PROTOBUF_VERIFY_VERSION;
+    std::cout << "ali was here\n";
+
+    // RPC::AppendEntries test_msg;
+
+    return 0;
+}
