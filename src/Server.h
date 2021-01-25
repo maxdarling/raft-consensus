4f361fb7b215949b5b18ac2ec1645801b286d8d3
Ali Saeed <8asaeed@gmail.com>
2021-01-24 20:04:21 -0600

    Added pseudocode to Server.cpp

--- src/Server.h
+++ src/Server.h
@@ -1,14 +1,17 @@
 #include <optional>
 #include <vector>
+// #include "Messenger.h"
 
 class Server {
   public:
+    Server();
     void run();
+
   private:
     // PERSISTENT STATE
     int _current_term {0};
     std::optional<int> _voted_for;
-    // log[] -> might store protobuf messages encapsulating the raw commands?
+    // _log[] -> might store protobuf messages encapsulating the raw commands?
 
     // VOLATILE STATE ON ALL SERVERS
     int _commit_index {0};
@@ -17,6 +20,12 @@ class Server {
     // VOLATILE STATE ON LEADERS
     std::vector<int> _next_index;
     std::vector<int> _match_index;
+
+    // ADDITIONAL STATE
+    bool _leader {false};
+
+    // UTIL
+    // Messenger _messenger;
 
     void RPC_handler();
     void leader_tasks();
--- src/server-main.cpp
+++ src/server-main.cpp
@@ -1,12 +1,13 @@
 // TODO(ali): get protobufs to work
 #include <iostream>
 #include "raft_RPCs.pb.h"
+#include "Server.h"
 
 int main() {
     GOOGLE_PROTOBUF_VERIFY_VERSION;
-    std::cout << "ali was here\n";
+    Server s;
 
-    RPC::AppendEntries test_msg;
+    s.run();
 
     return 0;
 }
