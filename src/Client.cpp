7321a819be41f030098f7ac746033f7a86f0a099
Ali Saeed <8asaeed@gmail.com>
2021-01-31 23:51:05 -0600

    The RAFT Shell is now operational!

--- src/Client.cpp
+++ src/Client.cpp
@@ -7,38 +7,46 @@ Client::Client(const sockaddr_in &clientAddr, const unordered_map<int, sockaddr_
       _addr(ntohs(clientAddr.sin_addr.s_addr)),
       _port(ntohs(clientAddr.sin_port)) {}
 
-void Client::run() {}
+void Client::run() {
+    std::cout << "---WELCOME TO RASH (RAFT SHELL)---\n";
+    for (;;) {
+        std::string cmd;
+        std::cout << "> ";
+        std::getline(std::cin, cmd);
+        std::cout << executeCommand(cmd) << "\n";
+    }
+}
 
-std::string Client::executeCommand(std::string command) {
+std::string Client::executeCommand(std::string cmd) {
     std::string serializedRequest;
     {
         RPC rpc;
         ClientRequest *cr = new ClientRequest();
-        cr->set_command(command);
+        cr->set_command(cmd);
         cr->set_client_addr(_addr);
         cr->set_client_port(_port);
         rpc.set_allocated_clientrequest_message(cr);
         serializedRequest = rpc.SerializeAsString();
     }
 
-    RPC server_response;
+    RPC serverResponse;
     do {
         _messenger.sendMessageToServer(_leaderID, serializedRequest);
 
-        std::optional<std::string> msg_opt;
+        std::optional<std::string> msgOpt;
         // TODO(ali): timeout and try a different server if this one crashes
         // maybe pull the Timer class into its own file and use it?
-        while (!msg_opt) msg_opt = _messenger.getNextMessage();
-        server_response.ParseFromString(*msg_opt);
+        while (!msgOpt) msgOpt = _messenger.getNextMessage();
+        serverResponse.ParseFromString(*msgOpt);
 
-        if (!server_response.has_clientrequest_message()) {
+        if (!serverResponse.has_clientrequest_message()) {
             std::cout << "received ill-formed msg from server\n";
             return {};
         }
 
-        _leaderID = server_response.clientrequest_message().leader_id();
-        std::cout << "we should go to " << _leaderID << "\n";
-    } while (!server_response.clientrequest_message().success());
+        _leaderID = serverResponse.clientrequest_message().leader_id();
+        // std::cout << "we should go to " << _leaderID << "\n";
+    } while (!serverResponse.clientrequest_message().success());
 
-    return server_response.clientrequest_message().output();
+    return serverResponse.clientrequest_message().output();
 }
