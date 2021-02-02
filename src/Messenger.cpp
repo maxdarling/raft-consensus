14bd74aec541ace840836483c6bb38aa106cc3fe
maxdarling <maxdarling11@gmail.com>
2021-02-01 21:44:26 -0800

    truncate to 80 lines, and delete comments

--- src/Messenger.cpp
+++ src/Messenger.cpp
@@ -5,9 +5,6 @@
 
 #include <iostream>
 
-using std::cout;
-using std::endl;
-
 const static int SHADOW_MESSAGE_ID = -190;
 
 bool sockaddr_in_cmp(const sockaddr_in a, const sockaddr_in b);
@@ -17,20 +14,21 @@ bool sockaddr_in_cmp(const sockaddr_in a, const sockaddr_in b);
  * 
  * Network message delimiting: 
  * A simple protocol is used for delimiting messages. Each communication leads
- * with 4 bytes - denoting message length - and follows with the message itself.
+ * with 4 bytes - denoting message length - and follows with the message
+ * itself.
  * 
- * Server re-connection:
- * To implement post-crash reconnection, servers send special internal 
- * messages to all other servers on initialization. These are dubbed "shadow 
- * messages", and contain only the sender's serverId. When received, they 
- * indicate that the sending server is running, and wants to be connected to.
- * For simplicity, these messages are always sent on initialization, whether
- * the server is starting for the "first time" or re-joining post-crash.
+ * Server re-connection: 
+ * To implement post-crash reconnection, servers send special internal messages
+ * to all other servers on initialization. These are dubbed "shadow messages",
+ * and contain only the sender's serverId. When received, they indicate that
+ * the sending server is running, and wants to be connected to. For simplicity,
+ * these messages are always sent on initialization, whether the server is
+ * starting for the "first time" or re-joining post-crash.
  */ 
 
 
 /** 
- * Background thread routine to process incoming messages. 
+ * Background thread routine to process incoming messages.
  * 
  * Client messages will be placed in the message queue, while shadow messages
  * will prompt a network connection to the sender.
@@ -43,18 +41,17 @@ void Messenger::collectMessagesRoutine() {
         int msgLength;
         int n = _networker->readAll(connfd, &msgLength, sizeof(msgLength));
         if (n != sizeof(msgLength)) {
-            cout << "readAll failed, message collection aborted" << endl;
             continue;
         }
-        msgLength = ntohl(msgLength); // convert back to host order before using 
+        msgLength = ntohl(msgLength); // convert to host order before use
 
         // handle shadow message
         if (msgLength == SHADOW_MESSAGE_ID) {
             // protocol: read the peer's 4-byte ID 
             int peerServerId;
-            n = _networker->readAll(connfd, &peerServerId, sizeof(peerServerId));
+            n = _networker->readAll(connfd, &peerServerId, 
+                                    sizeof(peerServerId));
             if (n != sizeof(peerServerId)) {
-                cout << "collectMessagesRoutine(): aborting message" << endl;
                 continue;
             }
             peerServerId = ntohl(peerServerId);
@@ -67,7 +64,6 @@ void Messenger::collectMessagesRoutine() {
             _serverIdToFd[peerServerId] =
                 _networker->establishConnection(_serverIdToAddr[peerServerId]);
             _closedConnections.erase(peerServerId);
-            cout << "successful ~~SHADOW~~ connection to server " << peerServerId << endl;
         }
 
         // handle default message 
@@ -75,7 +71,6 @@ void Messenger::collectMessagesRoutine() {
             char msgBuf [msgLength];
             n = _networker->readAll(connfd, msgBuf, sizeof(msgBuf));
             if (n != msgLength) {
-                cout << "readAll failed, message collection aborted" << endl;
                 continue;
             }
             std::lock_guard<std::mutex> lock(_m);
@@ -90,11 +85,13 @@ void Messenger::collectMessagesRoutine() {
  * Establishes connections to all other messenger servers in the given list.
  * Waits indefinitely until all outgoing connections are established. 
  */
-Messenger::Messenger(const int serverId, const unordered_map<int, struct sockaddr_in>& serverList) {
+Messenger::Messenger(const int serverId, 
+                     const unordered_map<int, sockaddr_in>& serverList) {
     _isClient = false;
     _serverId = serverId;
     _serverIdToAddr = serverList;
-    _clientAddrToFd = map<sockaddr_in, int, decltype(sockaddr_in_cmp)*>(&sockaddr_in_cmp);
+    _clientAddrToFd = 
+        map<sockaddr_in, int, decltype(sockaddr_in_cmp)*>(&sockaddr_in_cmp);
 
     // start a networker on our assigned port
     int port = ntohs(_serverIdToAddr[_serverId].sin_port);
@@ -108,11 +105,9 @@ Messenger::Messenger(const int serverId, const unordered_map<int, struct sockadd
     for (const auto& [peerId, peerAddr] : serverList) {
         if (peerId != _serverId) {
             int connfd;
-            while( (connfd = _networker->establishConnection(peerAddr)) == -1) {
-                cout << "failed to connect to server #" << peerId << endl;
+            while(-1 == (connfd = _networker->establishConnection(peerAddr))) {
                 sleep(3);
             }
-            cout << "successfully connected to server #" << peerId << endl;
             _serverIdToFd[peerId] = connfd;
 
             // send a shadow message to this peer
@@ -122,7 +117,6 @@ Messenger::Messenger(const int serverId, const unordered_map<int, struct sockadd
             _sendMessage(peerId, std::string(buf, sizeof(buf)), true);
         }
     }
-    cout << "Outbound connections completed on server " << _serverId << endl;
 }
 
 
@@ -131,10 +125,12 @@ Messenger::Messenger(const int serverId, const unordered_map<int, struct sockadd
  * 
  * Note: 'clientPort' should be in host-byte order. 
  */
-Messenger::Messenger(const unordered_map<int, sockaddr_in>& serverList, const int clientPort) {
+Messenger::Messenger(const unordered_map<int, sockaddr_in>& serverList, 
+                     const int clientPort) {
     _isClient = true;
     _serverIdToAddr = serverList;
-    _clientAddrToFd = map<sockaddr_in, int, decltype(sockaddr_in_cmp)*>(&sockaddr_in_cmp);
+    _clientAddrToFd = 
+        map<sockaddr_in, int, decltype(sockaddr_in_cmp)*>(&sockaddr_in_cmp);
 
     // start a networker on our assigned port
     _networker = new Networker(clientPort);
@@ -158,23 +154,27 @@ Messenger::~Messenger() {
  * 
  * Errors and closed connections are dealt with automatically.  
  */
-bool Messenger::_sendMessage(const int serverId, std::string message, bool isShadowMsg = false,
-                             bool isIntendedForClient = false, sockaddr_in clientAddr = {}) {
+bool Messenger::_sendMessage(const int serverId, std::string message, 
+                             bool isShadowMsg = false,
+                             bool isIntendedForClient = false, 
+                             sockaddr_in clientAddr = {}) {
     // for now: only manage closed connections for servers, not clients
     if (!isIntendedForClient && _closedConnections.count(serverId)) {
-        cout << "sendMessage(): server #" << serverId << " is bogus, or we closed it" << endl;
         return false;
     }
 
     int msgLength = (isShadowMsg) ? SHADOW_MESSAGE_ID : message.length();
     msgLength = htonl(msgLength); // convert to network order before sending
 
-    int connfd = (isIntendedForClient) ? _clientAddrToFd[clientAddr] : _serverIdToFd[serverId];
+    int connfd = (isIntendedForClient) ? 
+        _clientAddrToFd[clientAddr] : 
+        _serverIdToFd[serverId];
 
     // send message length and body
-    if (_networker->sendAll(connfd, &msgLength, sizeof(msgLength)) != sizeof(msgLength) ||
-        _networker->sendAll(connfd, message.c_str(), message.length()) != message.length()) {
-        cout << "sendMessage(): closing connection to server #" << serverId << endl;
+    if ((_networker->sendAll(connfd, &msgLength, sizeof(msgLength)) != 
+                                                sizeof(msgLength)) ||      
+        (_networker->sendAll(connfd, message.c_str(), message.length()) != 
+                                                     message.length())) {
         if (isIntendedForClient) {
             close(_clientAddrToFd[clientAddr]);
         } else {
@@ -193,10 +193,10 @@ bool Messenger::_sendMessage(const int serverId, std::string message, bool isSha
  */
 bool Messenger::sendMessageToServer(const int serverId, std::string message) {
     /* client case: client network connections are made here on-demand */
-    if (_isClient && !_serverIdToFd.count(serverId) || _closedConnections.count(serverId)) {
+    if (_isClient && 
+        !_serverIdToFd.count(serverId) || _closedConnections.count(serverId)) {
         int connfd = _networker->establishConnection(_serverIdToAddr[serverId]);
         if (connfd == -1) {
-            cout << "sendMessageToServer(): couldn't connect to server #" << serverId << endl;
             return false;
         }
         _serverIdToFd[serverId] = connfd;
@@ -214,11 +214,11 @@ bool Messenger::sendMessageToServer(const int serverId, std::string message) {
  * Note: theoretically, messages may be sent between clients, although not 
  * initially intended. 
  */ 
-bool Messenger::sendMessageToClient(const sockaddr_in clientAddr, std::string message) {
+bool Messenger::sendMessageToClient(const sockaddr_in clientAddr, 
+                                    std::string message) {
     if (!_clientAddrToFd.count(clientAddr)) {
         int connfd = _networker->establishConnection(clientAddr);
         if (connfd == -1) {
-            cout << "sendMessageToClient(): couldn't connect to client " << endl;
             return false;
         }
         _clientAddrToFd[clientAddr] = connfd;  
